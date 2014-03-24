1f0b1db2797953cad33d8553ec989330ed4f5fbf
da99 <i-hate-spam-1234567@mailinator.com>
2014-03-23 19:22:31 -0500

    Dev. checkpoint: Started: writing specs

--- specs/on_a_ladder.rb
+++ specs/on_a_ladder.rb
@@ -1,8 +1,61 @@
 
-describe "on_a_ladder" do
+class A
+end
 
-  it "runs" do
-    On_A_Ladder.should.flunk "No tests written."
+class B
+end
+
+class B1
+end
+
+class B2
+end
+
+class B3
+end
+
+class C
+end
+
+def keys o
+  o.keys.sort { |a, b| return a.to_s > b.to_s }
+end
+
+def parents o
+  keys(o).map { |k| o[k][:parent] }
+end
+
+describe ".Top" do
+
+  it "saves class" do
+    o = On_A_Ladder.new
+    .Top(A)
+    .Top(B)
+
+    keys(o.Ladder).should == [A, B]
   end
 
 end # === describe on_a_ladder ===
+
+describe ".Down" do
+
+  it "saves class" do
+    o = On_A_Ladder.new
+    .Top(A)
+    .Down(B)
+    .Down(C)
+
+    keys(o.Ladder).should == [A, B, C]
+  end
+
+  it "sets :parent to previous entry" do
+    o = On_A_Ladder.new
+    .Top(A)
+    .Down(B)
+    .Down(C)
+
+    parents(o.Ladder).should == [nil, B, C]
+  end
+
+end # === describe .Down ===
+
