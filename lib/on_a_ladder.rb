839ff61872d7defe58a1068e5bd82391b9eaeb07
da99 <i-hate-spam-1234567@mailinator.com>
2014-03-28 09:42:26 -0500

    Dev. checkpoint.

--- lib/on_a_ladder.rb
+++ lib/on_a_ladder.rb
@@ -17,14 +17,27 @@ class On_A_Ladder
 
     @ladder = @ladder.concat(r.parent_sql)
     @ladder << r
-    size = @ladder.size
+    last_index = @ladder.size - 1
+    sqls = []
     @ladder.each_with_index { |v, i|
-      case i
-      when 0           # top
-      when size - 1    # bottom
-      else             # middle
+      sqls << if i == 0 # top
+        "SELECT ? as class_id, id, NULL as parent_id
+        FROM :klass
+        WHERE id in (SELECT parent_id FROM :middle_cte)"
+      elsif i != last_index # middle
+        "SELECT ? AS class_id, id, ? AS parent_id
+        FROM :klass
+        WHERE id IN ( SELECT parent_id FROM :bottom_cte )"
+      else             # bottom
+        "SELECT ? AS class_id, id, ? AS parent_id
+        FROM :klass
+        WHERE id = ?"
       end
     }
+
+    sqls << I_Dig_Sql.new(sqls.map { |s|
+    }.join("UNION")).AS(NEW_NAME)
+
     self
   end
 
