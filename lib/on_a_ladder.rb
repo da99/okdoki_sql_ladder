53d95c06e331a13f364dea91534dbbf9090d03fe
da99 <i-hate-spam-1234567@mailinator.com>
2014-04-01 09:37:06 -0500

    Dev. checkpoint: Started: Extracing array travels logic from sql logic.

--- lib/on_a_ladder.rb
+++ lib/on_a_ladder.rb
@@ -1,4 +1,91 @@
 
+class About_Arr
+
+  class << self
+
+    def Back arr
+      size = arr.size
+      arr.reverse.each_with_index { |v, i|
+        real_index = (size - 1) - i
+        next_meta  =
+        prev_meta  =
+        meta.val = v
+        meta.index = real_index
+        meta.i     = i
+        meta.next  = next_meta
+        meta.prev  = prev_meta
+
+        meta.fin
+        yield meta.val, meta.index, meta
+      }
+    end
+
+    def Forward arr
+      size = arr.size
+      arr.each_with_index { |v, i|
+        meta = About_Arr.Meta.new
+        real_index   = i
+        next_meta    =
+        prev_meta    =
+        meta.val    = v
+        meta.index  = real_index
+        meta.i      = i
+        meta.next   = next_meta
+        meta.prev   = prev_meta
+
+        meta.fin
+        yield meta.val, meta.index, meta
+      }
+    end
+
+  end # === class self ===
+
+  class Meta
+
+    def initialize
+      @is_fin = false
+    end
+
+    [
+      :real_index,
+      :last_index,
+      :next_meta, :prev_meta,
+      :val, :index, :i, :next, :prev
+    ].each { |v|
+      eval %~
+        def #{v}
+          @#{v}
+        end
+
+        def #{v}= o
+          raise "No more values can be set after .fin is called" if @is_fin
+          @#{v} = o
+          return o
+        end
+      ~
+    }
+
+    def fin
+      @is_fin = true
+      self
+    end
+
+    def top?
+      real_index == 0
+    end
+
+    def middle?
+      real_index != 0 && real_index != last_index
+    end
+
+    def bottom?
+      real_index == last_index
+    end
+
+  end # === class Meta ===
+
+end # === class About_Arr ===
+
 module On_A_Ladder
 
   def ladder o
@@ -7,16 +94,12 @@ module On_A_Ladder
     curr = o.parent_sql
     sqls = [curr]
     while (curr = curr.parent_sql) do
-      sqls.push curr
+      sqls.unshift curr
     end
 
-    last_index = sqls.length - 1
-
-    sqls.each_with_index { |v, i|
-
-      cte_table_name      = "#{o.class.to_s.downcase}_ladder_#{i-last_index}"
-      prev_cte_table_name = "#{o.class.to_s.downcase}_ladder_#{i-last_index-1}"
-      prev                = sqls[i-1]
+    # ---------------------------------------------------
+    About_Seq.Back(sqls) { |v, i, meta|
+      cte_table_name      = "#{o.class.to_s.downcase}_ladder_#{i}"
 
       new_sql = case v
                 when I_Dig_Sql
@@ -25,17 +108,17 @@ module On_A_Ladder
                   klass = v.first
                   fkey = v[1] || 'parent_id'
 
-                  if i == last_index # TOP
+                  if meta.first? # TOP
                     I_Dig_Sql.new("SELECT ? as class_id, id, NULL as parent_id
                       FROM #{klass.table_name}
-                      WHERE id in (SELECT parent_id FROM #{prev_cte_table_name})", klass.class_id)
+                      WHERE id in (SELECT parent_id FROM #{meta.prev[:cte_table_name]})", klass.class_id)
 
-                  elsif i != last_index && i != 0 # MIDDLE
+                  elsif meta.middle? # MIDDLE
                     I_Dig_Sql.new("SELECT ? AS class_id, id, ? AS parent_id
                       FROM #{klass.table_name}
-                      WHERE id IN ( SELECT parent_id FROM #{prev_cte_table_name} )", klass.class_id, fkey)
+                      WHERE id IN ( SELECT parent_id FROM #{meta.prev[:cte_table_name]} )", klass.class_id, fkey)
 
-                  else i == 0 # BOTTOM
+                  else meta.bottom? # BOTTOM
                     I_Dig_Sql.new("SELECT ? AS class_id, id, ? AS parent_id
                       FROM #{klass.table_name}
                       WHERE id = ?", klass.class_id, fkey, o.id)
@@ -48,7 +131,9 @@ module On_A_Ladder
 
       new_sql.tag_as(tag)
       new_sql
+
     }
+    # ---------------------------------------------------
 
     self.WITH(
       I_Dig_Sql.new(sqls.find_tagged(tag).map { |s| 
